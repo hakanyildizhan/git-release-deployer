b200b8db7ce6bac4635a281a65bf78d5107e15ce
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2022-02-09 15:14:42 +0300

    Fix paths in tests

--- src/ReleaseDeployerService.UnitTest/TestConfigWriteReleaseDate.cs
+++ src/ReleaseDeployerService.UnitTest/TestConfigWriteReleaseDate.cs
@@ -18,7 +18,7 @@ namespace ReleaseDeployerService.UnitTest
         public void Write_Release_Date_To_Config_File_With_No_Release_Date_Config_Is_Valid(string releaseDate)
         {
             var date = DateTime.ParseExact(releaseDate, "yyyy-MM-ddTHH:mm:ssZ", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AdjustToUniversal);
-            string filePath = "ConfigFiles\\testconfig1.xml";
+            string filePath = Path.Combine("ConfigFiles", "testconfig1.xml");
             string newFilePath = "testconfig1_copy.xml";
             File.Copy(filePath, newFilePath, true);
             IConfigReader configReader = new XmlConfigReader(newFilePath);
@@ -33,7 +33,7 @@ namespace ReleaseDeployerService.UnitTest
         [DataRow("2022-01-31T12:33:57Z")]
         public void Update_Release_Date_With_Newer_Date_Config_Is_Valid(string releaseDate)
         {
-            string filePath = "ConfigFiles\\testconfig2.xml";
+            string filePath = Path.Combine("ConfigFiles", "testconfig2.xml");
             string newFilePath = "testconfig2_copy1.xml";
             File.Copy(filePath, newFilePath, true);
             IConfigReader configReader = new XmlConfigReader(newFilePath);
@@ -48,7 +48,7 @@ namespace ReleaseDeployerService.UnitTest
         [TestMethod]
         public void Update_Release_Date_With_Same_Date_Do_Nothing()
         {
-            string filePath = "ConfigFiles\\testconfig2.xml";
+            string filePath = Path.Combine("ConfigFiles", "testconfig2.xml");
             string newFilePath = "testconfig2_copy2.xml";
             File.Copy(filePath, newFilePath, true);
             IConfigReader configReader = new XmlConfigReader(newFilePath);
@@ -63,7 +63,7 @@ namespace ReleaseDeployerService.UnitTest
         public void Update_Release_Date_With_Older_Date_Do_Nothing(string olderDate)
         {
             var date = DateTime.ParseExact(olderDate, "yyyy-MM-ddTHH:mm:ssZ", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AdjustToUniversal);
-            string filePath = "ConfigFiles\\testconfig2.xml";
+            string filePath = Path.Combine("ConfigFiles", "testconfig2.xml");
             string newFilePath = "testconfig2_copy2.xml";
             File.Copy(filePath, newFilePath, true);
             IConfigReader configReader = new XmlConfigReader(newFilePath);
