0a8aaa7503d139f602e55ef14f330b97d07bad54
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2022-02-09 15:20:02 +0300

    Workflow to build solution (#1)
    * Workflow to build solution
    
    * Add solution path
    
    * Add restore path
    
    * Fix paths in tests

--- src/ReleaseDeployerService.UnitTest/TestConfigReading.cs
+++ src/ReleaseDeployerService.UnitTest/TestConfigReading.cs
@@ -12,35 +12,40 @@ namespace ReleaseDeployerService.UnitTest
         [TestMethod]
         public void Valid_Config_Without_LastReleaseDate_Is_Read_Correctly()
         {
-            IConfigReader configReader = new XmlConfigReader("ConfigFiles\\testconfig1.xml");
+            string configFile = Path.Combine("ConfigFiles", "testconfig1.xml");
+            IConfigReader configReader = new XmlConfigReader(configFile);
             Assert.IsTrue(configReader.CheckValidity());
         }
 
         [TestMethod]
         public void Valid_Config_With_LastReleaseDate_Is_Read_Correctly()
         {
-            IConfigReader configReader = new XmlConfigReader("ConfigFiles\\testconfig2.xml");
+            string configFile = Path.Combine("ConfigFiles", "testconfig2.xml");
+            IConfigReader configReader = new XmlConfigReader(configFile);
             Assert.IsTrue(configReader.CheckValidity());
         }
 
         [TestMethod]
         public void Config_Missing_ServiceConfig_Node_Cannot_Be_Read()
         {
-            IConfigReader configReader = new XmlConfigReader("ConfigFiles\\testconfig3.xml");
+            string configFile = Path.Combine("ConfigFiles", "testconfig3.xml");
+            IConfigReader configReader = new XmlConfigReader(configFile);
             Assert.IsFalse(configReader.CheckValidity());
         }
 
         [TestMethod]
         public void Config_Missing_Deploy_Type_Cannot_Be_Read()
         {
-            IConfigReader configReader = new XmlConfigReader("ConfigFiles\\testconfig4.xml");
+            string configFile = Path.Combine("ConfigFiles", "testconfig4.xml");
+            IConfigReader configReader = new XmlConfigReader(configFile);
             Assert.IsFalse(configReader.CheckValidity());
         }
 
         [TestMethod]
         public void Config_Incorrect_Interval_Cannot_Be_Read()
         {
-            IConfigReader configReader = new XmlConfigReader("ConfigFiles\\testconfig5.xml");
+            string configFile = Path.Combine("ConfigFiles", "testconfig5.xml");
+            IConfigReader configReader = new XmlConfigReader(configFile);
             Assert.IsFalse(configReader.CheckValidity());
         }
     }
